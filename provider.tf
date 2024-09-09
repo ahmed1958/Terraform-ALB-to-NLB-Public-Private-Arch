dd332561c870665a911f46ff5ba6f87014621009
ahmed sayed <ahmed195069@feng.bu.edu.eg>
2024-09-09 22:20:12 +0300

    remote state file is done

--- provider.tf
+++ provider.tf
@@ -1,3 +1,8 @@
+provider "aws"{
+    shared_config_files = ["/home/ahmed/.aws/config"]
+    shared_credentials_files =["/home/ahmed/.aws/credentials"]
+    profile = "admin"
+}
 terraform {
   required_providers {
     aws = {
@@ -5,11 +10,37 @@ terraform {
       version = "~> 5.64.0"
     }
   }
+     backend "s3" {
+        bucket = "ahmed-state-terraform-yat"
+        key = "dev/terraform.tfstate"
+        region = "us-east-1"
+        dynamodb_table = "terraform-up-and-running-locks"
+        encrypt = true
+    }
 }
 
-provider "aws"{
-    shared_config_files = ["/home/ahmed/.aws/config"]
-    shared_credentials_files =["/home/ahmed/.aws/credentials"]
-    profile = "admin"
+resource "aws_s3_bucket" "terraform-state" {
+    bucket = "ahmed-state-terraform-yat"
+    lifecycle {
+      prevent_destroy = true
+    }
+}
+
+resource "aws_s3_bucket_versioning" "s3-enable-versioning" {
+  bucket = "ahmed-state-terraform-yat"
+  versioning_configuration {
+    status = "Enabled"
+  }
+}
+
+resource "aws_dynamodb_table" "terraform-lock-table" {
+  name = "terraform-up-and-running-locks"
+  billing_mode = "PAY_PER_REQUEST"
+  hash_key = "LockID"
+  attribute {
+    name = "LockID"
+    type = "S"
+  }
+
 }
 
